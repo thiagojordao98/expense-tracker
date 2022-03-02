00b68b9344592ab5b1c035be47a122dadddb1ee0
Thiago Jordão <thiagojordao98@gmail.com>
2022-03-01 23:50:19 -0300

    add mock data

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  { date: new Date(2022, 2, 1), category: 'food', title: 'McDonalds', value: 32.12 },
+  { date: new Date(2022, 2, 3), category: 'food', title: 'Subway', value: 19.90 },
+  { date: new Date(2022, 2, 2), category: 'rent', title: 'Aluguel', value: 800 },
+  { date: new Date(2022, 2, 1), category: 'salary', title: 'Salário ACME', value: 32.12 }
+];
